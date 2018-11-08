59e11073dc677f4df2c907ddb20b52547046c78e
David Simon <davidsimon@swellinvesting.com>
2018-11-08 11:08:59 -0800

    Added floats to prop test

--- spec/rison_cycle_spec.rb
+++ spec/rison_cycle_spec.rb
@@ -21,6 +21,7 @@ end
 def scalar_input_property
   freq(
     [3, :integer],
+    [2, :float],
     [3, :string],
     [1, :boolean],
     [1, [:literal, nil]],
@@ -28,9 +29,11 @@ def scalar_input_property
 end
 
 def array_input_property(depth)
-  array {
-    input_property(depth)
-  }
+  Deflating.new(
+    array {
+      input_property(depth)
+    }
+  )
 end
 
 def hash_input_property(depth)
@@ -40,14 +43,25 @@ def hash_input_property(depth)
 end
 
 def encodable_form(data)
-  data
+  case data
+  when Deflating then data.array.map{|elem| encodable_form(elem) }
+  when Hash then data.map{|key, val| [key, encodable_form(val)] }.to_h
+  else data
+  end
 end
 
 def expected_decoded_form(data)
-  data
+  case data
+  when Deflating then data.array.map{|elem| expected_decoded_form(elem) }
+  when Hash then data.map{|key, val| [key, expected_decoded_form(val)] }.to_h
+  when Float then Rational(data.to_s)
+  else data
+  end
 end
 
 describe 'Rison dumping and loading' do
+  make_my_diffs_pretty!
+
   it "recreates data through dump and load" do
     property_of { input_property }.check do |data|
       encoded = Rison.dump(encodable_form(data))
